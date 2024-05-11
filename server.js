e64a3448b029038620852a86d87d0c9dc83e9eb5
Nikolai <niklonberg@gmail.com>
2024-05-11 20:31:24 +0200

    add update route

--- server.js
+++ server.js
@@ -40,17 +40,31 @@
 /************************************************ version 3 ************************************************/
 
 const http = require("http");
-const { getProducts, getProduct, createProduct } = require("./controllers/productController");
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+} = require("./controllers/productController");
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/api/products" && req.method === "GET") {
     getProducts(req, res);
-  } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
-    const productID = req.url.split('/')[3]
+  } else if (
+    req.url.match(/\/api\/products\/([0-9]+)/) &&
+    req.method === "GET"
+  ) {
+    const productID = req.url.split("/")[3];
     getProduct(req, res, productID);
-  } else if (req.url === "/api/products" && req.method === 'POST') {
-    createProduct(req, res)
+  } else if (req.url === "/api/products" && req.method === "POST") {
+    createProduct(req, res);
+  } else if (
+    req.url.match(/\/api\/products\/([0-9]+)/) &&
+    req.method === "PUT"
+  ) {
+    const productID = req.url.split("/")[3];
+    updateProduct(req, res, productID);
   } else {
     res.writeHead(404, { "Content-type": "application/json" });
     res.end(JSON.stringify({ message: "route not found" }));
